9ecab4e978ad825ba5cce061477d9c076077f2ea
christy313 <christycreativity@gmail.com>
2022-10-10 20:59:43 -0400

    Set up basic structure

--- 004-ip-tracker/src/App.js
+++ 004-ip-tracker/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div bg-image>
+        <div>bg-image</div>
+        <div>search bar</div>
+      </div>
+      <div>info</div>
+      <div>map</div>
+    </div>
+  );
 };
 
 export default App;
